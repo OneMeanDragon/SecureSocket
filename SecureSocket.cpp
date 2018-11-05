62e2e59558f96eed8ff7c980e5808c55c2c5692d
OneMeanDragon <dragonkings@gmail.com>
2018-11-05 17:49:01 -0500

    Update SecureSocket.cpp

--- SecureSocket.cpp
+++ SecureSocket.cpp
@@ -1,5 +1,19 @@
 #include "SecureSocket.h"
 
+DWORD SecureSocket::SocketProcess(LPVOID param) { //(WorkerThread)
+	SecureSocket *mSocket = reinterpret_cast<SecureSocket*>(param);
+	DWORD EventID;
+
+	while (mSocket->m_connected != FALSE)
+	{
+		//EventID = WaitForSingleObject(mSocket->events[0], INFINITE);
+		//how do i send this function to/on the parent thread
+		//mSocket->SocketAPCProcess(param, EventID);
+	}
+
+	return 0;
+}
+
 void SecureSocket::SetupSchannelCredentials(UINT32 protocol, SCHANNEL_CRED &schannelcredentials)
 {
 	schannelcredentials.dwVersion = SCHANNEL_CRED_VERSION;
@@ -8,46 +22,57 @@ void SecureSocket::SetupSchannelCredentials(UINT32 protocol, SCHANNEL_CRED &scha
 	schannelcredentials.dwFlags |= SCH_CRED_MANUAL_CRED_VALIDATION;
 }
 
-SecureSocket::SecureSocket()
+void SecureSocket::Connect(std::string serv, std::string sec_serv, UINT16 serv_port)
 {
-	WSAStartup(0x0202, &versioninfo);
-	// Load Security DLL
-	securitydllmodule = LoadLibrary("Secur32.dll");
-	// Initialize Schannel
-	INIT_SECURITY_INTERFACE initsecurtyinterfacefunction = (INIT_SECURITY_INTERFACE)GetProcAddress(securitydllmodule, "InitSecurityInterfaceA");
-	PSecurityFunctionTable schannel = initsecurtyinterfacefunction();
-	if (!schannel) {
-		MessageBox(0, "Failed to initialize schannel", "Message", MB_TASKMODAL | MB_OK);
-		//clean up?
-	} else {
-		MessageBox(0, "initialized schannel", "Message", MB_TASKMODAL | MB_OK);
+	//are we trying to connect an already open socket?
+	if (this->mySocket != INVALID_SOCKET) { 
+		//clean up secure bits...
+		//
+		//clean up old socket
+		closesocket(this->mySocket); 
+		this->m_connected = FALSE; 
+		this->mySocket = INVALID_SOCKET; 
 	}
-	// Setup Schannel Credentials
-	ZeroMemory(&m_scc, sizeof(m_scc));
-	SetupSchannelCredentials(m_protocol, m_scc);
-	// Get Client Credentials handle
-	SECURITY_STATUS securitystatus = schannel->AcquireCredentialsHandleA(
-		0,
-		(SEC_CHAR *)UNISP_NAME_A,
-		SECPKG_CRED_OUTBOUND,
-		0,
-		&m_scc,
-		0,
-		0,
-		&m_cc,
-		0
-		);
-	
-	if (securitystatus != SEC_E_OK)
-		MessageBox(0, "Failed to get credenetials", "Message", MB_TASKMODAL | MB_OK);
-	else
-		MessageBox(0, "Got client credenetials", "Message", MB_TASKMODAL | MB_OK);
-}
 
-SecureSocket::~SecureSocket()
-{
-	//free the library
-	FreeLibrary(securitydllmodule);
-	securitydllmodule = NULL;
+	ServerAddress = serv;
+	ServerSecAddress = sec_serv;
+	port = serv_port;
+
+	int iResult = getaddrinfo(this->ServerAddress.c_str(), std::to_string(this->port).c_str(), &hints, &result);
+	if (iResult != 0) { 
+		//this->Error("getaddrinfo failed.", true); //TODO: Event
+		return; 
+	}
+	ptr = result;
+	for (ptr = result; ptr != NULL; ptr = ptr->ai_next)
+	{
+		//this->AttemptingConnectionTo(ptr); //TODO: Event
+
+		this->mySocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+		if (this->mySocket == INVALID_SOCKET) {
+			continue;
+		}
+		iResult = connect(this->mySocket, ptr->ai_addr, (int)(ptr->ai_addrlen));
+		if (iResult != SOCKET_ERROR)
+		{
+			break; //break the for loop were connected
+		}
+		else {
+			closesocket(this->mySocket);
+			this->mySocket = INVALID_SOCKET;
+		}
+	}
+	freeaddrinfo(result);
+
+	if (this->mySocket == INVALID_SOCKET)
+	{
+		//connection failed
+		//this->Error("Server connection Failed!", true); //TODO: Event
+	}
+	else {
+		//we connected
+		this->m_connected = true;
+		time(&this->m_connecteddate);
+		//Connected(); //TODO: Event
+	}
 }
-	
