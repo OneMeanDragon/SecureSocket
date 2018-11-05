8bca6e3a18d7153520a935dab79b86c1d1c0ca07
OneMeanDragon <dragonkings@gmail.com>
2018-11-05 17:48:40 -0500

    Update SecureSocket.h

--- SecureSocket.h
+++ SecureSocket.h
@@ -1,11 +1,15 @@
 #pragma once
-#include <Windows.h>
 #pragma comment(lib, "ws2_32.lib")
+
+#include <WinSock2.h>
+#include <Windows.h>
+#include <winuser.h>
+#include <ws2tcpip.h>
 #define SECURITY_WIN32
 #include <SChannel.h>
 #include <security.h>
 
-
+#include <chrono>
 #include <string>
 
 class SecureSocket
@@ -15,7 +19,12 @@ private:
 	std::string ServerSecAddress = "";
 	UINT16 port = 0;
 
+	time_t m_connecteddate = NULL;
+
 	SOCKET mySocket = INVALID_SOCKET;
+	BOOL m_connected = FALSE;
+	struct addrinfo *result, *ptr, hints;
+
 
 	WSADATA versioninfo;
 	HMODULE securitydllmodule = NULL;
@@ -27,8 +36,49 @@ private:
 	void SetupSchannelCredentials(UINT32 protocol, SCHANNEL_CRED &schannelcredentials);
 
 public: 
+	static DWORD WINAPI SocketProcess(LPVOID param);
 
-  SecureSocket();
-  ~SecureSocket();
-  
-  };
+	
+	SecureSocket()
+	{
+		WSAStartup(0x0202, &versioninfo);
+		// Load Security DLL
+		securitydllmodule = LoadLibrary("Secur32.dll");
+		// Initialize Schannel
+		INIT_SECURITY_INTERFACE initsecurtyinterfacefunction = (INIT_SECURITY_INTERFACE)GetProcAddress(securitydllmodule, "InitSecurityInterfaceA");
+		PSecurityFunctionTable schannel = initsecurtyinterfacefunction();
+		if (!schannel) {
+			MessageBox(0, "Failed to initialize schannel", "Message", MB_TASKMODAL | MB_OK);
+			//clean up?
+		} else {
+			MessageBox(0, "initialized schannel", "Message", MB_TASKMODAL | MB_OK);
+		}
+		// Setup Schannel Credentials
+		ZeroMemory(&m_scc, sizeof(m_scc));
+		SetupSchannelCredentials(m_protocol, m_scc);
+		// Get Client Credentials handle
+		SECURITY_STATUS securitystatus = schannel->AcquireCredentialsHandleA(
+			0,
+			(SEC_CHAR *)UNISP_NAME_A,
+			SECPKG_CRED_OUTBOUND,
+			0,
+			&m_scc,
+			0,
+			0,
+			&m_cc,
+			0
+		);
+		if (securitystatus != SEC_E_OK)
+			MessageBox(0, "Failed to get credenetials", "Message", MB_TASKMODAL | MB_OK);
+		else
+			MessageBox(0, "Got client credenetials", "Message", MB_TASKMODAL | MB_OK);
+	}
+	~SecureSocket()
+	{
+		//free the library
+		FreeLibrary(securitydllmodule);
+		securitydllmodule = NULL;
+	}
+	
+	void Connect(std::string serv, std::string sec_serv, UINT16 serv_port) ;
+};
