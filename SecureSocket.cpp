229e42da48e887789f88b8ee1a6d1802afa4e251
OneMeanDragon <dragonkings@gmail.com>
2018-11-05 03:08:53 -0500

    Update SecureSocket.cpp

--- SecureSocket.cpp
+++ SecureSocket.cpp
@@ -8,3 +8,46 @@ void SecureSocket::SetupSchannelCredentials(UINT32 protocol, SCHANNEL_CRED &scha
 	schannelcredentials.dwFlags |= SCH_CRED_MANUAL_CRED_VALIDATION;
 }
 
+SecureSocket::SecureSocket()
+{
+	WSAStartup(0x0202, &versioninfo);
+	// Load Security DLL
+	securitydllmodule = LoadLibrary("Secur32.dll");
+	// Initialize Schannel
+	INIT_SECURITY_INTERFACE initsecurtyinterfacefunction = (INIT_SECURITY_INTERFACE)GetProcAddress(securitydllmodule, "InitSecurityInterfaceA");
+	PSecurityFunctionTable schannel = initsecurtyinterfacefunction();
+	if (!schannel) {
+		MessageBox(0, "Failed to initialize schannel", "Message", MB_TASKMODAL | MB_OK);
+		//clean up?
+	} else {
+		MessageBox(0, "initialized schannel", "Message", MB_TASKMODAL | MB_OK);
+	}
+	// Setup Schannel Credentials
+	ZeroMemory(&m_scc, sizeof(m_scc));
+	SetupSchannelCredentials(m_protocol, m_scc);
+	// Get Client Credentials handle
+	SECURITY_STATUS securitystatus = schannel->AcquireCredentialsHandleA(
+		0,
+		(SEC_CHAR *)UNISP_NAME_A,
+		SECPKG_CRED_OUTBOUND,
+		0,
+		&m_scc,
+		0,
+		0,
+		&m_cc,
+		0
+		);
+	
+	if (securitystatus != SEC_E_OK)
+		MessageBox(0, "Failed to get credenetials", "Message", MB_TASKMODAL | MB_OK);
+	else
+		MessageBox(0, "Got client credenetials", "Message", MB_TASKMODAL | MB_OK);
+}
+
+SecureSocket::~SecureSocket()
+{
+	//free the library
+	FreeLibrary(securitydllmodule);
+	securitydllmodule = NULL;
+}
+	
